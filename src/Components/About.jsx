9cd982c0488b1cd3557625e5fef58f88a8cd3622
hugogaytan10 <estardus9@gmail.com>
2023-07-23 20:38:37 -0600

    fs

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,8 +7,8 @@ export const About = () => {
         <div className='md:w-1/3'>
             <p className='font-extrabold mt-10'>Hola, soy Hugo Gaytán</p>
             <div className='h-96 overflow-y-auto mt-5 relative'>
-                <span className='absolute w-1 bg-cyan-600 h-96 left-4 md:left-0'></span>
-                <p className='text-gray-500 font-thin text-justify md:p-2 '>El mundo está en constante evolución gracias a los diseñadores que ponen al usuario en el centro de su proceso. Los creativos diseñan una experiencia a través de una serie de puntos de contacto que crean el marco para tener interacciones significativas. Todo comienza con comprender las necesidades del usuario y concebir una solución innovadora para satisfacerlas.</p>
+                <span className='absolute w-1 bg-cyan-600 h-96 left-0'></span>
+                <p className='text-gray-500 font-thin text-justify p-2 '>El mundo está en constante evolución gracias a los diseñadores que ponen al usuario en el centro de su proceso. Los creativos diseñan una experiencia a través de una serie de puntos de contacto que crean el marco para tener interacciones significativas. Todo comienza con comprender las necesidades del usuario y concebir una solución innovadora para satisfacerlas.</p>
             </div>
         </div>
     </div>
